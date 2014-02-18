b28d10b1815b41c469011b00e78855f22b69e952
Geoffrey Oxberry <goxberry@gmail.com>
2014-02-17 21:30:14 -0500

    Add Ex 6, EC 2 of LRTHW.
    Add Exercise 6, Extra Credit 2 of Learn Ruby the Hard Way. Points out
    all places where string is put inside a string.

---
+++ learn-ruby-the-hard-way/ex6-ec2.rb
@@ -0,0 +1,25 @@
+x = "There are #{10} types of people."
+binary = "binary"
+do_not = "don't"
+# There are two strings put inside a string in the next line.
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+# In each of the next two lines, a string is put inside a string.
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
+# In total, there are only four places where a string is put inside string in
+# this program.
