2030bef7c2b2f201decf732c4a4f455d7d6d2331
Geoffrey Oxberry <goxberry@gmail.com>
2014-02-19 02:08:26 -0500

    Add Ex 10, EC 1 of LRTHW.
    Add list of Ruby escape sequences in comments.

---
+++ learn-ruby-the-hard-way/ex10-ec1.rb
@@ -0,0 +1,13 @@
+# Escape sequences in Ruby:
+# \" double quote
+# \' single quote
+# \\ single backslash
+# \a bell/alert
+# \b backspace
+# \r carriage return
+# \n newline
+# \s space
+# \t tab
+# \{octal number between 0000 and 0377} represents given byte
+# \{hex number between 0x00 and 0xFF} represents given byte
+# See a full list at http://readruby.io/strings
