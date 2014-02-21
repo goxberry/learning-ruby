a17c5dcaa8f103b43e31f6308aa50cc61248c70d
Geoffrey Oxberry <goxberry@gmail.com>
2014-02-21 14:25:49 -0500

    Confirm completion of Ex 13, EC 1 of LRTHW.
    Record completion of Exercise 13, Extra Credit 1 of Learn Ruby the
    Hard Way by copying the output of running the script with two
    arguments.

---
+++ learn-ruby-the-hard-way/ex13-ec1.rb
@@ -0,0 +1,9 @@
+# If fewer than three arguments are given to the script:
+#
+# oxberry$ ruby ex13.rb 1 2
+# The script is called: ex13.rb
+# Your first variable is: 1
+# Your second variable is: 2
+# Your third variable is:
+#
+# The missing arguments are empty strings, I think.
