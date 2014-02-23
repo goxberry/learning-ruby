b7b0e3cd3c385b9b78c8ee116c794f455e5dd3fd
Geoffrey Oxberry <goxberry@gmail.com>
2014-02-22 23:43:12 -0800

    Confirm completion of Ex 14, EC 1 of LRTHW.
    Confirm completion of Exercise 14, Extra Credit 1 of Learn Ruby the
    Hard Way. Played Zork. It was okay.

---
+++ learn-ruby-the-hard-way/ex14-ec1.rb
@@ -0,0 +1,4 @@
+# Zork is a text-based adventure game.
+# You can find a copy online and play it in your browser.
+# If you're like me, you'll also need to search online for help on how to
+# play the game.
